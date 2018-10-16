efcd11c904bb1b40657d8664f606b4f5e7e0764e
lgrgic <lukagrgic14@gmail.com>
2018-10-16 13:51:28 +0200

    inheritance & derived classes

--- palindrome.rb
+++ palindrome.rb
@@ -1,6 +1,33 @@
-# Returns true for a palindrome, false otherwise.
+# Defines a Phrase class (inheriting from String).
+class Phrase < String
 
-def palindrome?(string)
-  proccessed_content = string.downcase
-  proccessed_content == proccessed_content.reverse
+  # Processes the string for palindrome testing.
+  def processor(string)
+    string.downcase
+  end
+
+  # Returns content for palindrome testing.
+  def processed_content
+    processor(self)
+  end
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+end
+
+# Defines a translated Phrase.
+class TranslatedPhrase < Phrase
+  attr_accessor :translation
+
+  def initialize(content, translation)
+    super(content)
+    @translation = translation
+  end
+
+  # Processes the translation for palindrome testing.
+  def processed_content
+    processor(translation)
+  end
 end
